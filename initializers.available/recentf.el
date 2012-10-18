fa4ebe13d2649864eb0c7e4c3483e3c5eaed7df8
Tim Harper <timcharper@gmail.com>
2012-10-18 17:50:55 -0600

    recentf initializer

--- initializers.available/recentf.el
+++ initializers.available/recentf.el
@@ -1,1 +1,11 @@
 (setq recentf-max-saved-items 100)
+(recentf-mode)
+
+(defun recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
