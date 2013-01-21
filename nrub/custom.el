c6a074be9d46a66b85f4e6bb4372d048ce486685
Paul English <paul@onfrst.com>
2013-01-21 16:04:35 -0700

    Set default font-size for nrub

--- nrub/custom.el
+++ nrub/custom.el
@@ -85,6 +85,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:height 85 :family "Monaco"))))
  '(cursor ((t (:background "White"))))
  '(hl-line ((t (:inherit highlight :background "#302500")))))
 
