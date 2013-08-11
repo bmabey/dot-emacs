c7d3e27efcce83bc1da9ca9e5de497683d7c280a
Ben Mabey <ben@benmabey.com>
2013-08-11 15:28:36 -0600

    upgrades yasnippets

--- initializers.available/yasnippet.el
+++ initializers.available/yasnippet.el
@@ -1,6 +1,7 @@
 (ensure-packages-installed '(yasnippet))
 (require 'yasnippet)
+(yas-global-mode 1)
 
-(setq yas/root-directory (concat dotfiles-dir "snippets"))
-(yas/initialize)
-(yas/load-directory (concat dotfiles-dir "snippets"))
+;; (setq yas/root-directory (concat dotfiles-dir "snippets"))
+;; (yas/initialize)
+;; (yas/load-directory (concat dotfiles-dir "snippets"))
