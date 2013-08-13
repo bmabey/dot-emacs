795a68d810bcaaa71868f8e79ef0624d49f0fd49
Ben Mabey <ben@benmabey.com>
2013-08-13 16:40:08 -0600

    let RET and SPC always go to emacs and not evil (nice for full-ack)

--- initializers.available/evil.el
+++ initializers.available/evil.el
@@ -19,11 +19,21 @@
 (define-key evil-normal-state-map (kbd "M-.") nil)
 (define-key evil-normal-state-map (kbd "M-.") nil)
 
+
 (global-set-key (kbd "C-]") 'evil-find-char)
 (global-set-key (kbd "C-M-]") 'evil-find-char-backward)
 
 (global-set-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
 (global-set-key (kbd "C-c -") 'evil-numbers/dec-at-pt)
-
-
 ;;(add-hook 'emacs-lisp-mode-hook 'evil-paredit-mode)
+
+;; Making the most of RET and SPC (via http://www.emacswiki.org/emacs/Evil#toc11)
+;; It is common for Emacs modes like these to define key bindings for RET and SPC. Since these are motion commands, Evil ;; places its key bindings for these in evil-motion-state-map. However, these commands are fairly worthless to a ;; ;; ;; seasoned Vim user, since they do the same thing as j and l commands. Thus it is useful to remove them from
+;; evil-motion-state-map so as when modes define them, RET and SPC bindings are available directly.
+
+(defun evil/move-key (keymap-from keymap-to key)
+  "Moves key binding from one keymap to another, deleting from the old location. "
+  (define-key keymap-to key (lookup-key keymap-from key))
+  (define-key keymap-from key nil))
+(evil/move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
+(evil/move-key evil-motion-state-map evil-normal-state-map " ")
